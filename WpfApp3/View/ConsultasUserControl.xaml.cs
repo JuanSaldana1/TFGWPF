40f1b5a8db0c4d99d36a2e8e19dba0a117eee9a1
Juan Saldaña <saldana.juan.2002@gmail.com>
2022-05-12 10:18:44 +0200

    Consultas como base

--- WpfApp3/View/ConsultasUserControl.xaml.cs
+++ WpfApp3/View/ConsultasUserControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
+using MaterialDesignThemes.Wpf;
 using WpfApp3.ViewModel;
 
 namespace WpfApp3.View;
@@ -8,8 +10,21 @@ public partial class ConsultasUserControl {
     InitializeComponent();
   }
 
-  private void ButtonSelectAllProductsOnClick(object sender, RoutedEventArgs e) {
+  /*private void ButtonSelectAllProductsOnClick(object sender, RoutedEventArgs e) {
     var productViewModel = new ProductViewModel();
     ListViewResultados.DataContext = new ProductViewModel();
+  }*/
+
+  private void Sample5_DialogHost_OnDialogClosing(object sender, DialogClosingEventArgs eventArgs) {
+    Debug.WriteLine($"SAMPLE 5: Closing dialog with parameter: {eventArgs.Parameter ?? string.Empty}");
+
+    //you can cancel the dialog close:
+    //eventArgs.Cancel();
+
+    if (!Equals(eventArgs.Parameter, true))
+      return;
+
+    if (!string.IsNullOrWhiteSpace(AnimalTextBox.Text))
+      AnimalListBox.Items.Add(AnimalTextBox.Text.Trim());
   }
 }
