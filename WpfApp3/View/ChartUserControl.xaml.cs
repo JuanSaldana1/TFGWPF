0be2d8928bf57a07f97a97dce9922c3f608e662a
Juan Saldaña <saldana.juan.2002@gmail.com>
2022-05-25 10:24:35 +0200

    You can save Charts as images

--- WpfApp3/View/ChartUserControl.xaml.cs
+++ WpfApp3/View/ChartUserControl.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 using System.Windows.Media;
 using LiveCharts;
 using LiveCharts.Wpf;
@@ -6,8 +7,11 @@ using LiveChartsCore;
 using LiveChartsCore.Kernel.Sketches;
 using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore.SkiaSharpView.Painting;
+using LiveChartsCore.SkiaSharpView.SKCharts;
 using SkiaSharp;
+using WpfApp3.Model;
 using Axis = LiveChartsCore.SkiaSharpView.Axis;
+using MessageBox = ModernWpf.MessageBox;
 
 namespace WpfApp3.View;
 
@@ -15,6 +19,8 @@ public partial class ChartUserControl {
   private static readonly SKColor s_blue = new(25, 118, 210);
   private static readonly SKColor s_red = new(229, 57, 53);
   private static readonly SKColor s_yellow = new(198, 167, 0);
+  public ChartValues<PostModel> Results { get; set; }
+  public Func<double, string> MillionFormatter { get; set; }
   public SeriesCollection SeriesCollection { get; set; }
   public string[] Labels { get; set; }
   public Func<double, string> YFormatter { get; set; }
@@ -61,14 +67,14 @@ public partial class ChartUserControl {
   public ISeries[] TodosLosGraficos { get; set; } = {
     new ColumnSeries<int> {
       Name = "Comentarios",
-      Values = new[] {21, 16, 4, 28, 14, 33, 72, 24, 19, 0, 5, 48, 13, 7, 14, 13, 13, 13, 1, 1, 2, 2, 2},
+      Values = new[] {21, 16, 4, 28, 14, 33, 72, 24, 19, 5, 48, 13, 7, 14, 13, 13, 13, 14, 8, 3, 1, 1, 2, 1},
       ScalesYAt = 0, // it will be scaled at the Axis[0] instance
       Fill = new SolidColorPaint(s_red, 2),
     },
     new LineSeries<int> {
       LineSmoothness = 1,
       Name = "Entradas (posts)",
-      Values = new[] {10, 16, 20, 17, 7, 5, 17, 3, 5, 0, 7, 4, 1, 2, 3, 5, 4, 6, 1, 1, 2, 2, 2},
+      Values = new[] {10, 16, 20, 17, 7, 5, 17, 3, 5, 1, 7, 4, 1, 2, 3, 5, 4, 6, 1, 1, 2, 2, 2},
       Stroke = new SolidColorPaint(s_blue, 2),
       GeometrySize = 10,
       GeometryStroke = new SolidColorPaint(s_blue, 2),
@@ -78,10 +84,13 @@ public partial class ChartUserControl {
   };
 
   public ISeries[] GraficoPublicaciones { get; set; } = {
-    new LineSeries<double> {
+    new LineSeries<int> {
       LineSmoothness = 1,
       Name = "Entradas (posts)",
-      Values = new double[] {10, 16, 20, 17, 7, 5, 17, 3, 5, 1, 7, 4, 1, 2, 3, 5, 4, 6, 1, 1, 2, 2, 2},
+      Values = new[] {
+        10, 16, 20, 17, 7, 5, 17, 3, 5, 0, 1, 7, 4, 1, 2, 3, 5, 4, 6, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0
+      },
       Stroke = new SolidColorPaint(s_blue, 2),
       GeometrySize = 10,
       GeometryStroke = new SolidColorPaint(s_blue, 2),
@@ -90,12 +99,39 @@ public partial class ChartUserControl {
     },
   };
 
-  public Axis[] XAxes { get; set; } = {
+  public ISeries[] GraficoComentarios { get; set; } = {
+    new ColumnSeries<double> {
+      Name = "Comentarios",
+      Values = new double[] {
+        21, 16, 4, 28, 14, 33, 72, 24, 19, 0, 5, 48, 13, 7, 14, 13, 13, 13, 14, 0, 0, 0, 0, 8, 3, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 1, 0, 0, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0
+      },
+      Stroke = new SolidColorPaint(s_yellow, 2),
+      Fill = null,
+      ScalesYAt = 0 // it will be scaled at the Axis[0] instance 
+    },
+  };
+
+  public Axis[] XAxesWithPublications { get; set; } = {
     new Axis {
       Name = "Meses",
       NamePaint = new SolidColorPaint {Color = SKColors.Black},
       // Use the labels property for named or static labels 
-      Labels = new string[] {
+      Labels = new[] {
+        "Nov-13", "Dic-13", "Ene-14", "Feb-14", "Mar-14", "Abr-14", "May-14", "Jun-14", "Jul-14", "Ago-14", "Sep-14",
+        "Oct-14", "Nov-14", "Dic-14", "Ene-15", "Feb-15", "Mar-15", "Abr-15", "May-15", "Oct-15", "Nov-15", "Nov-16",
+        "Feb-17", "Mar-17", "Mar-18"
+      },
+      LabelsRotation = 15
+    }
+  };
+
+  public Axis[] XAxesAllMonths { get; set; } = {
+    new Axis {
+      Name = "Meses",
+      NamePaint = new SolidColorPaint {Color = SKColors.Black},
+      // Use the labels property for named or static labels 
+      Labels = new[] {
         "Nov-13", "Dic-13", "Ene-14", "Feb-14", "Mar-14", "Abr-14", "May-14", "Jun-14", "Jul-14", "Ago-14", "Sep-14",
         "Oct-14", "Nov-14", "Dic-14", "Ene-15", "Feb-15", "Mar-15", "Abr-15", "May-15", "Jun-15", "Jul-15", "Ago-15",
         "Sep-15", "Oct-15", "Nov-15", "Dic-15", "Ene-16", "Feb-16", "Mar-16", "Abr-16", "May-16", "Jun-16", "Jul-16",
@@ -117,4 +153,20 @@ public partial class ChartUserControl {
       LabelsPaint = new SolidColorPaint(s_blue),
     },
   };
+
+  private void CreateImageFromCartesianControl() {
+    try {
+      // you can take any chart in the UI, and build an image from it 
+      var skChart = new SKCartesianChart(CartesianChart) {Width = 1200, Height = 900,};
+      skChart.SaveImage("Estadísticas publicaciones.png");
+    }
+    catch (Exception e) {
+      MessageBox.Show(e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+      throw;
+    }
+  }
+
+  private void ButtonCreateChartImage_OnClick(object sender, RoutedEventArgs e) {
+    CreateImageFromCartesianControl();
+  }
 }
